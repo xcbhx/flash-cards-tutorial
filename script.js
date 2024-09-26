b532638b86e46ff21eb308abb7c99838c3672fd4
xchbx <ceina.ellison@gmail.com>
2024-09-26 10:45:20 -0400

    flipping the card

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 import data from './data.js'
 
-console.log(data.title)
-console.log(data.description)
-console.log(data.questions)
-
 const titleH1 = document.querySelector('#title')
-console.log(titleH1)
-
-titleH1.innerHTML = data.title
-
 const descriptionP = document.querySelector('#description')
-console.log(descriptionP)
-
-descriptionP.innerHTML = data.description
-
 const scoreP = document.querySelector('#score')
-
-const questionContainer = document.querySelector('#flip-card')
-
 const qCardP = document.querySelector('#q-card > p')
 const aCardP = document.querySelector('#a-card > p')
+const questionContainer = document.querySelector('#flip-card')
 
+let questions = data.questions
 let questionIndex = -1
+
+titleH1.innerHTML = data.title
+descriptionP.innerHTML = data.description
 
 const showNextQuestion = () => {
     questionIndex += 1
-    questionIndex = questionIndex % (questions.length - 1)
+    // questions.length used to avoid skipping the last question
+    // (questions.length - 1) limits the cycle between the first two questions, skipping last one. 
+    questionIndex = questionIndex % questions.length
     qCardP.innerHTML = questions[questionIndex].q
     aCardP.innerHTML = questions[questionIndex].a
-
-    scoreP.innerHTML = `Score ${questionIndex + 1}/${questions.length - 1}`
-}
+    
+    scoreP.innerHTML = `Score ${questionIndex + 1}/${questions.length}`
+  }
 
 showNextQuestion()
+
+document.body.addEventListener('click', e => {
+    console.log(e)
+    if (e.target.matches('#answer-button')) {
+      questionContainer.classList.add('flip')
+    } else if (e.target.matches('#question-button')) {
+      questionContainer.classList.remove('flip')
+    } else if (e.target.matches('#next-button')) {
+      questionContainer.classList.remove('flip')
+      showNextQuestion()
+    }
+  })
